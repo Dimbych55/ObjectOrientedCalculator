4bf1a9bb814b6470ab3c599f85a59c324ae65c4c
Megaproger5467 <mr.korol.drift@gmail.com>
2020-07-10 19:58:11 +0600

    вебинтерфейс

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using Calculate;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Operation = new SelectListItem[]
+            {
+        new SelectListItem() { Value = "Sum", Text ="сложение" },
+        new SelectListItem() { Value = "razn", Text ="вычитание" },
+        new SelectListItem() { Value = "del", Text ="деление" },
+        new SelectListItem() { Value = "umnog", Text ="умножение" },
+        new SelectListItem() { Value = "step", Text ="степень" },
+        new SelectListItem() { Value = "square", Text ="квадрат" }
+            };
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index(double a, double b, string operation)
+        {
+            Operationscalculate C;
+            C = new Operationscalculate();
+            double result;
+           
+            
+            result = C.Sum(a, b);
+            result = C.razn(a, b);
+            result = C.del(a, b);
+            result = C.umnog(a, b);
+            result = C.step(a, b);
+            result = C.square(a);
+            
+            ViewBag.Result = result;
+
+            ViewBag.Operation = new SelectListItem[]
+            {
+        new SelectListItem() { Value = "Sum", Text ="сложение" },
+        new SelectListItem() { Value = "razn", Text ="вычитание" },
+        new SelectListItem() { Value = "del", Text ="деление" },
+        new SelectListItem() { Value = "umnog", Text ="умножение" },
+        new SelectListItem() { Value = "step", Text ="степень" },
+        new SelectListItem() { Value = "square", Text ="квадрат" }
+            };
+            return View();
+        }
+
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
